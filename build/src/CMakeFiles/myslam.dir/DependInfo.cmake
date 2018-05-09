46402f2dc44747ad9ac752a28938236f6de4f3b6
xzh <xuzihan.boy@163.com>
2018-05-09 14:08:21 +0800

    use version 0.3

---
+++ build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/camera.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/config.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/frame.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/g2o_types.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/g2o_types.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/map.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/mappoint.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/xzh/Documents/SLAM/slambook/project/0.3/src/visual_odometry.cpp" "/home/xzh/Documents/SLAM/slambook/project/0.3/build/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
